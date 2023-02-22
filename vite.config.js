ae532fedea8baf6641ad90a52ab1b4d7745d4b51
= <=>
2023-02-22 11:10:11 -0600

    Deploy 3

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://AletzMan.github.io/hangman_game",
   plugins: [react()],
 })
